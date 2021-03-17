76ffabd04fe686e36784e834ff5008132aebc2b4
Uditsing Khati <uditsing.k@yudiz.in>
2021-03-17 16:34:14 +0530

    Change SexyBaccarat Betting Details Response.

--- Code/Webet333.models/Response/Game/SexyBaccarat/SexyBaccaratBettingDetailsResponse.cs
+++ Code/Webet333.models/Response/Game/SexyBaccarat/SexyBaccaratBettingDetailsResponse.cs
@@ -16,17 +16,17 @@ namespace Webet333.models.Response.Game.SexyBaccarat
     {
         public string GameType { get; set; }
 
-        public long WinAmount { get; set; }
+        public decimal WinAmount { get; set; }
 
-        public DateTimeOffset TxTime { get; set; }
+        public DateTime TxTime { get; set; }
 
         public string GameInfo { get; set; }
 
-        public long RealWinAmount { get; set; }
+        public decimal RealWinAmount { get; set; }
 
-        public DateTimeOffset UpdateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
 
-        public long RealBetAmount { get; set; }
+        public decimal RealBetAmount { get; set; }
 
         public string UserId { get; set; }
 
@@ -36,7 +36,7 @@ namespace Webet333.models.Response.Game.SexyBaccarat
 
         public long TxStatus { get; set; }
 
-        public long BetAmount { get; set; }
+        public decimal BetAmount { get; set; }
 
         public string PlatformTxId { get; set; }
 
@@ -44,13 +44,13 @@ namespace Webet333.models.Response.Game.SexyBaccarat
 
         public string Currency { get; set; }
 
-        public long JackpotWinAmount { get; set; }
+        public decimal JackpotWinAmount { get; set; }
 
         public long Id { get; set; }
 
-        public long JackpotBetAmount { get; set; }
+        public decimal JackpotBetAmount { get; set; }
 
-        public long Turnover { get; set; }
+        public decimal Turnover { get; set; }
 
         public string RoundId { get; set; }
     }
@@ -71,7 +71,7 @@ namespace Webet333.models.Response.Game.SexyBaccarat
 
         public string DealerDomain { get; set; }
 
-        public long WinLoss { get; set; }
+        public decimal WinLoss { get; set; }
 
         public string Status { get; set; }
     }
