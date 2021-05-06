4b501e0307ca5123035f7bcbb4dadcd516355d2b
yudiz.raju <rajugiri.g@yudiz.in>
2021-05-06 17:09:34 +0530

    Page Navigation Completed in Layout

---
+++ Code/Webet333.middleware/Pages/Web/download.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Webet333.middleware.Pages.Web
+{
+    public class downloadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
