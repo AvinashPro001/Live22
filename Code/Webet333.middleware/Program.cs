b67dc9ca8ba5b8d6e594a69a6271edc3268f80b6
SaiyedTosif <tosif.s@yudiz.in>
2021-06-02 10:37:13 +0530

    slick working on click

--- Code/Webet333.middleware/Program.cs
+++ Code/Webet333.middleware/Program.cs
@@ -19,6 +19,6 @@ namespace Webet333.middleware
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://localhost:5000/").UseContentRoot(Directory.GetCurrentDirectory());
     }
 }
