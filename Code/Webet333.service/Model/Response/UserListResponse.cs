96638729caf91a3ea546eddda169846c199cc83d
Uditsing Khati <uditsing.k@yudiz.in>
2021-06-08 21:45:29 +0530

    -> Created "YeeBet" register services.

---
+++ Code/Webet333.service/Model/Response/UserListResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Webet333.service.Model
+{
+    public class Datum
+    {
+        public string Username { get; set; }
+
+        public string GameName { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Prefix { get; set; }
+    }
+
+    public class UserListResponse
+    {
+        public string message { get; set; }
+
+        public List<Datum> data { get; set; }
+    }
+}
---
+++ Code/Webet333.service/Model/Response/UsersListResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Webet333.service.Model
+{
+    public class UsersListResponse
+    {
+        public string Username { get; set; }
+
+        public string GameName { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Prifix { get; set; }
+    }
+}
