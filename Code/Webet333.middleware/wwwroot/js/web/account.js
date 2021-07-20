44b4da4f3e05a736705f13b3024a0a670a1607be
Rajugiri Goshwamee <rajugiri.g@yudiz.in>
2021-07-20 19:58:32 +0530

    Add Home banner in web

--- Code/Webet333.middleware/wwwroot/js/web/account.js
+++ Code/Webet333.middleware/wwwroot/js/web/account.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
                 if (data == null) {
                     SetSessionStorage("siteData", Encryption(JSON.stringify(SiteData)));
                     await AllPromotionCallAPI();
+                    await HomeBannerCallAPI();
                     await AllAnnouncementsCallAPI();
                     await CallDownloadLinkAPI();
                     await CallAPIForBankPages();
@@ -36,6 +37,7 @@ $(document).ready(function () {
                 }
                 else {
                     if (data.PromotionPageData == null) { await AllPromotionCallAPI(); SetPromotionInPromotionPage(); }
+                    if (data.HomeBannerData == null) { await HomeBannerCallAPI(); SetHomeBannerInMainPage(); }
                     if (data.AnnouncementsData == null) { await AllAnnouncementsCallAPI(); SetAnnouncementsOnAllPages(); }
                     if (data.WalletData == null) { await GetWalletList(); }
                     if (data.AdminBankPageData == null) { await CallAPIForBankPages(); SetAdminBankPage() }
