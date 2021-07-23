d40e5d35b8127115476ff0ba7e015793ae98ed91
Uditsing Khati <uditsing.k@yudiz.in>
2021-07-23 15:49:13 +0530

    -> Added UAT details.

--- Code/Webet333.admin/ClientApp/src/environments/environment.ts
+++ Code/Webet333.admin/ClientApp/src/environments/environment.ts
@@ -1,14 +1,26 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+let baseUrl = '', baseUrlWithoutVersion = '';
+let environmentName = 'DEBUG';  // 'DEBUG' OR 'STAG'
+let PortAPI = '9001';
+
+switch (environmentName.toUpperCase()) {
+    case 'DEBUG':
+        baseUrl = 'https://uatapi.wb3api.com/api/v1/';
+        baseUrlWithoutVersion = 'https://uatapi.wb3api.com';
+        break;
+    case 'STAG':
+        baseUrl = 'https://api.wb3api.com/api/v1/';
+        baseUrlWithoutVersion = 'https://api.wb3api.com';
+        break;
+    case 'LOCAL':
+        baseUrl = 'http://127.0.0.1:' + PortAPI + '/api/v1/';
+        baseUrlWithoutVersion = 'http://127.0.0.1:' + PortAPI;
+        break;
+}
 
 export const environment = {
     production: false,
-    //apiUrl: 'https://api.wb3api.com/api/v1/',
-    //apiUrlWithVersion: 'https://api.wb3api.com/'
-    apiUrl: 'http://127.0.0.1:9001/api/v1/',
-    apiUrlWithVersion: 'http://127.0.0.1:9001/'
+    apiUrl: baseUrl,
+    apiUrlWithVersion: baseUrlWithoutVersion
 };
 
 export const account = {
