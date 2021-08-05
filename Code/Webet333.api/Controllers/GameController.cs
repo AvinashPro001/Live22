7c5973577e7a40a020aeaae0cf199e4f5056040d
Uditsing Khati <uditsing.k@yudiz.in>
2021-08-05 22:51:08 +0530

    -> Getting betting details after 20-30 min.

--- Code/Webet333.api/Controllers/GameController.cs
+++ Code/Webet333.api/Controllers/GameController.cs
@@ -1390,7 +1390,7 @@ namespace Webet333.api.Controllers
 
             PragmaticBettingDetailsRequest request = new PragmaticBettingDetailsRequest
             {
-                StartTimeStamp = date.AddMinutes(-1)
+                StartTimeStamp = date.AddMinutes(-30)   //  Betting details getting after 20 - 30 min.
             };
 
             var response = await GamePlayGameHelpers.CallBettingDetailsAPI(request);
