e29e1ea4a3db0adcc73c9d47b24e49141df3055f
Rajugiri Goshwamee <rajugiri.g@yudiz.in>
2021-08-17 19:08:44 +0530

    Code Commit

--- Code/Webet333.middleware/wwwroot/js2/constants/apiConsts.js
+++ Code/Webet333.middleware/wwwroot/js2/constants/apiConsts.js
@@ -180,5 +180,5 @@ var apiEndPoints = {
 
     checkUsernameExists: 'account/check/username/exists',
 
-    getUsername: '/api/v1/account/get_username_by_id'
+    getUsername: 'api/v1/account/get_username_by_id'
 };
